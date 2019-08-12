e5520e8d5bfe30a570830bd0753ca9ce6478fe60
eunwon <tjdwhd0221@naver.com>
2019-08-12 16:24:55 +0900

    Glide를 이용한 Splash 입니다.

---
+++ Splash.java
@@ -0,0 +1,34 @@
+package com.ebookfrenzy.weeklyweather;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ImageView imageView = findViewById(R.id.wea);
+        Glide.with(this).load(R.raw.weather).apply(new RequestOptions().circleCrop().centerCrop()).into(imageView);
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
+    }
+}
